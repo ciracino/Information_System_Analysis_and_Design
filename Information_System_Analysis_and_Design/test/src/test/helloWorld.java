a73cacc018ad63a360730615f0baec332a691755
Hamzzi_2o2 <ciracino88@gmail.com>
2026-03-09 15:48:35 +0900

    test

---
+++ Information_System_Analysis_and_Design/test/src/test/helloWorld.java
@@ -0,0 +1,12 @@
+package test;
+
+public class helloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
+
+// 함수 추출하기 - 가독성, 재사용성, 유지보수
+// 가독성 - 함수 이름만 보고도 기능을 유추할 수 있게
+// 재사용성 - 어디서든 사용가능하게
+// 유지보수 - 유지보수하는 상황에서도 의도가 이해되는 네이밍 사용
