c90481f60e4cff705ac4e55615a6052eca30ce7e
Hamzzi_2o2 <ciracino88@gmail.com>
2026-03-11 15:40:58 +0900

    의존관계, 찰나적 관계, 일반화 관계

---
+++ Information_System_Analysis_and_Design/test/src/week01/HomeAppliance.java
@@ -0,0 +1,15 @@
+package week01;
+
+public abstract class HomeAppliance {
+	private int no;
+	private String manufacturer;
+	private int years;
+	
+	public void turnOn() {
+		
+	}
+	
+	public void turnOff() {
+		
+	}
+}
---
+++ Information_System_Analysis_and_Design/test/src/week01/washer.java
@@ -0,0 +1,15 @@
+package week01;
+
+// washer 클래스는 HomeAppliance 클래스를 상속함.
+// HomeAppliance 는 washer 와 일반화 관계에 있음.
+
+// 연관 관계 - 교수와 학생의 관계
+// 찰나적 관계 - 교수와 피자 배달원의 관계
+
+public class washer extends HomeAppliance {
+	// 생성자
+	public washer(int no, String manufacturer, int years) {
+		super();
+	}
+	
+}
