c63b90b84bdf4a6e7dfcf821cf0fd2b61f721e33
Hamzzi_2o2 <ciracino88@gmail.com>
2026-03-11 14:54:40 +0900

    클래스 다이어그램을 코드로 옮기는 방법

---
+++ Information_System_Analysis_and_Design/test/src/week01/course.java
@@ -0,0 +1,24 @@
+package week01;
+
+//course
+//-id: string
+//-name: string
+//-numberOfStudent: int
+//+addStudent
+//+deleteStudent
+
+// 위와 같이 클래스 다이어그램을 작성하면 아래와 같이 코드를 작성한다.
+
+class Course {
+	private String id;
+	private String name;
+	private int numberOfStudent;
+	
+	public void addStudent() {
+		
+	}
+	
+	public void deleteStudent() {
+		
+	}
+}
